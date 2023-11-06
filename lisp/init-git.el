3677c8687db99a2d3cade6dabc3ed217441699f5
Yejun Su <goofan.su@gmail.com>
2023-11-06 18:21:47 +0800

    add skip-ci and push all remotes options to magit

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,6 +2,19 @@
   :pin nongnu
   :config
   (message "magit is loaded")
+
+  ;; git push with skip-ci option
+  (transient-append-suffix 'magit-push "-n"
+    '("-s" "Skip CI" "--push-option=skip-ci"))
+
+  ;; git push to all remotes
+  (transient-append-suffix 'magit-push "e"
+    '("E" "everywhere" magit-push-all))
+  (defun magit-push-all (&optional args)
+    (interactive (list (magit-push-arguments)))
+    (dolist (remote (magit-list-remotes))
+      (magit-push-to-remote remote args)))
+
   :custom
   (magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
 
