2647f8258d63f10a4a6f1b459a639d26a30b094e
Yejun Su <goofan.su@gmail.com>
2023-11-06 14:31:34 +0800

    Revert "set evil to insert state for git-commit-mode"
    This reverts commit f4644b579ca58f38792c25b7a385c59367351828.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -3,8 +3,6 @@
   :config
   (message "magit is loaded")
   :custom
-  (magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
-  :hook
-  (git-commit-mode . evil-insert-state))
+  (magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
 
 (provide 'init-git)
