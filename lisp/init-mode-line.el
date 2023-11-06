acbe5da6779393abc7705180e94a09e72c8ffcb3
Yejun Su <goofan.su@gmail.com>
2023-11-06 17:43:07 +0800

    anzu

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -7,4 +7,17 @@
   (message "doom-modeline is loaded")
   (doom-modeline-mode 1))
 
+(use-package anzu
+  :pin nongnu
+  :defer 1
+  :config
+  (message "anzu is loaded")
+  (global-anzu-mode))
+
+(use-package evil-anzu
+  :pin nongnu
+  :after (evil anzu)
+  :config
+  (message "evil-anzu is loaded"))
+
 (provide 'init-mode-line)
