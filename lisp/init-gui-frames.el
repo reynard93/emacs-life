4dc43d773c6da06255f921262ecefef55e43f3cc
Yejun Su <goofan.su@gmail.com>
2023-11-06 18:26:46 +0800

    start Emacs server with server-name "gui"

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -2,4 +2,14 @@
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 
+(use-package server
+  :when (display-graphic-p)
+  :ensure nil
+  :defer 1
+  :init
+  (setq server-name "gui")
+  :config
+  (unless (server-running-p)
+    (server-start)))
+
 (provide 'init-gui-frames)
