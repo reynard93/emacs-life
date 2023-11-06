622da63b25055a1be8cd277d8644ae200fcf51da
Yejun Su <goofan.su@gmail.com>
2023-11-06 12:12:48 +0800

    doom-modeline

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -1,4 +1,10 @@
 (column-number-mode 1)
 (display-time-mode 1)
 
+(use-package doom-modeline
+  :pin melpa
+  :config
+  (message "doom-modeline is loaded")
+  (doom-modeline-mode 1))
+
 (provide 'init-mode-line)
