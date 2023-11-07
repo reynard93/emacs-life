e8cce4689e66abac45b0fab4e3d840a23c2071c2
Yejun Su <goofan.su@gmail.com>
2023-11-07 15:10:34 +0800

    cleanup whitespaces

--- lisp/init-sourcehut.el
+++ lisp/init-sourcehut.el
@@ -1,18 +1,18 @@
-(defun yejun/paste-region-or-buffer (&optional p)                                              
-  (interactive "P")                                                                            
-  (let ((filename (read-string "Enter filename: " (buffer-name)))                              
-        (output-buffer " *paste-output*")                                                      
-        (public (if p " --visibility public" "")))                                             
-    (shell-command-on-region                                                                   
-     (if (use-region-p) (region-beginning) (point-min))                                        
-     (if (use-region-p) (region-end) (point-max))                                              
-     (concat "hut paste create --name \"" filename "\"" public)                                
-     output-buffer)                                                                            
-    (with-current-buffer output-buffer                                                         
-      (goto-char (point-max))                                                                  
-      (forward-line -1)                                                                        
-      (kill-new (thing-at-point 'line)))                                                       
-    (kill-buffer output-buffer)))                                                              
+(defun yejun/paste-region-or-buffer (&optional p)
+  (interactive "P")
+  (let ((filename (read-string "Enter filename: " (buffer-name)))
+        (output-buffer " *paste-output*")
+        (public (if p " --visibility public" "")))
+    (shell-command-on-region
+     (if (use-region-p) (region-beginning) (point-min))
+     (if (use-region-p) (region-end) (point-max))
+     (concat "hut paste create --name \"" filename "\"" public)
+     output-buffer)
+    (with-current-buffer output-buffer
+      (goto-char (point-max))
+      (forward-line -1)
+      (kill-new (thing-at-point 'line)))
+    (kill-buffer output-buffer)))
 
 (global-set-key (kbd "C-c b p") #'yejun/paste-region-or-buffer)
 
