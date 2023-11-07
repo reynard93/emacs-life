5fbd1e2936038c79dc3f43518b814b8cc956a6f9
Yejun Su <goofan.su@gmail.com>
2023-11-07 11:39:36 +0800

    tempel

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -1,4 +1,5 @@
 (use-package corfu
+  :defer 1
   :config
   (message "corfu is loaded")
   (global-corfu-mode)
@@ -6,5 +7,20 @@
   (corfu-cycle t)
   (corfu-auto t)
   (corfu-scroll-margin 5))
+
+(use-package tempel
+  :defer 1
+  :config
+  (message "tempel is loaded")
+  :custom
+  (tempel-trigger-prefix "<")
+  :hook
+  ((conf-mode prog-mode text-mode) . tempel-setup-capf))
+
+(defun tempel-setup-capf ()
+  (interactive)
+  (setq-local completion-at-point-functions
+              (cons #'tempel-expand
+                    completion-at-point-functions)))
 
 (provide 'init-corfu)
