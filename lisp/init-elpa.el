3f520d6487cad5fa28df847a44a8a64b3aaab4da
Yejun Su <goofan.su@gmail.com>
2023-11-07 20:48:47 +0800

    update init-elpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,11 +1,11 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+(setq package-enable-at-startup nil)
+(package-initialize)
+
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 (setq use-package-always-pin "gnu")
-
-(setq package-enable-at-startup nil)
-(package-initialize)
 
 (provide 'init-elpa)
