992f2dd429a8aec1be03b65dc2993e0cb710d18e
Yejun Su <goofan.su@gmail.com>
2023-11-08 12:17:04 +0800

    chatgpt-shell bindings

--- lisp/init-chatgpt.el
+++ lisp/init-chatgpt.el
@@ -1,12 +1,18 @@
 (use-package chatgpt-shell
   :pin melpa
-  :defer t
   :config
   (message "chatgpt-shell is loaded")
   :custom
   (chatgpt-shell-welcome-function nil)
   (chatgpt-shell-model-version (cl-position "gpt-4" chatgpt-shell-model-versions :test 'string=))
-  (chatgpt-shell-openai-key (lambda () (auth-source-pick-first-password :host "api.openai.com"))))
+  (chatgpt-shell-openai-key (lambda () (auth-source-pick-first-password :host "api.openai.com")))
+  :bind (("C-c z z" . chatgpt-shell)
+         ("C-c z b" . chatgpt-shell-prompt)
+         ("C-c z c" . chatgpt-shell-prompt-compose)
+         ("C-c z s" . chatgpt-shell-send-region)
+         ("C-c z S" . chatgpt-shell-send-and-review-region)
+         ("C-c z e" . chatgpt-shell-explain-code)
+         ("C-c z r" . chatgpt-shell-refactor-code)))
 
 (push '("*chatgpt*"
         (display-buffer-in-direction)
