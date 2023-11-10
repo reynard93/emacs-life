3e9940db6441c3635af0b136c551c5dc1a0cf202
Yejun Su <goofan.su@gmail.com>
2023-11-10 11:13:36 +0800

    s-x behaves M-x

--- lisp/init-macos-keybindings.el
+++ lisp/init-macos-keybindings.el
@@ -3,12 +3,12 @@
       mac-right-option-modifier 'none)
 
 (global-set-key (kbd "C-s-f") #'toggle-frame-fullscreen) ; Toggle fullscreen
+(global-set-key (kbd "s-x") #'execute-extended-command)  ; M-x
 (global-set-key (kbd "s-o") #'find-file)                 ; Open file
 (global-set-key (kbd "s-s") #'save-buffer)               ; Save file
 (global-set-key (kbd "s-w") #'kill-this-buffer)          ; Close file
 (global-set-key (kbd "s-a") #'mark-whole-buffer)         ; Select All
 (global-set-key (kbd "s-c") #'kill-ring-save)            ; Copy
-(global-set-key (kbd "s-x") #'kill-region)               ; Cut
 (global-set-key (kbd "s-v") #'yank)                      ; Paste
 (global-set-key (kbd "s-z") #'undo)                      ; Undo
 
