0d302805c4508fb9202f214c90089f767a19f40c
Yejun Su <goofan.su@gmail.com>
2023-11-13 11:32:20 +0800

    use tab as workspace

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -1,6 +1,18 @@
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
 (menu-bar-mode -1)
+
+(use-package scroll-bar
+  :ensure nil
+  :config
+  (message "scroll-bar is loaded")
+  (set-scroll-bar-mode nil))
+
+(use-package tab-bar
+  :ensure nil
+  :config
+  (message "tab-bar is loaded")
+  :custom
+  (tab-bar-show 1))
 
 (use-package ace-window
   :config
@@ -10,6 +22,7 @@
   :bind ([remap other-window] . ace-window))
 
 (use-package winner
+  :ensure nil
   :config
   (message "winner is loaded")
   (winner-mode 1))
--- lisp/init-macos-keybindings.el
+++ lisp/init-macos-keybindings.el
@@ -6,11 +6,20 @@
 (global-set-key (kbd "s-x") #'execute-extended-command)  ; M-x
 (global-set-key (kbd "s-o") #'find-file)                 ; Open file
 (global-set-key (kbd "s-s") #'save-buffer)               ; Save file
-(global-set-key (kbd "s-w") #'kill-this-buffer)          ; Close file
-(global-set-key (kbd "s-a") #'mark-whole-buffer)         ; Select All
+(global-set-key (kbd "s-a") #'mark-whole-buffer)         ; Select all
 (global-set-key (kbd "s-c") #'kill-ring-save)            ; Copy
 (global-set-key (kbd "s-v") #'yank)                      ; Paste
 (global-set-key (kbd "s-z") #'undo)                      ; Undo
+
+(global-set-key (kbd "s-t") #'tab-new)                   ; New tab
+(global-set-key (kbd "s-T") #'tab-undo)                  ; Undo tab
+(global-set-key (kbd "s-w") #'tab-close)                 ; Close tab
+(global-set-key (kbd "s-W") #'tab-close-group)           ; Close tab group
+(global-set-key (kbd "s-g") #'tab-switch)                ; Switch to other tab
+(global-set-key (kbd "s-G") #'tab-group)                 ; Add current tab to group
+(global-set-key (kbd "C-<tab>") #'tab-recent)            ; Switch between latest tabs
+(global-set-key (kbd "s-{") #'tab-previous)              ; Previous tab
+(global-set-key (kbd "s-}") #'tab-next)                  ; Next tab
 
 (defun yejun/move-beginning-of-line ()
   (interactive)
