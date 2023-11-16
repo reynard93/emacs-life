ae5823bf31ac847feeb1c6413d850b6eda2f22f5
Yejun Su <goofan.su@gmail.com>
2023-11-16 09:12:34 +0800

    denote custom package functions with a plus sign to avoid conflicts

--- lisp/init-lib.el
+++ lisp/init-lib.el
@@ -3,7 +3,7 @@
   (let ((project (project-current nil dir)))
     (project-find-file-in nil nil project)))
 
-(defun project--root-dir (&optional dir)
+(defun +project-root-dir (&optional dir)
   (if-let ((project (project-current nil dir)))
       (project-root project)
     nil))
@@ -11,7 +11,7 @@
 (defun yejun/search-project (&optional dir thing)
   (interactive)
   (consult-ripgrep
-   (project--root-dir dir)
+   (+project-root-dir dir)
    (when thing (thing-at-point thing))))
 
 (defun yejun/search-project-for-symbol-at-point ()
@@ -48,7 +48,7 @@ file path, otherwise, get a full file path with
   "Save the relative buffer path into the kill-ring.
 The path is relative to `project-current'."
   (interactive)
-  (yejun/yank-buffer-path nil (project--root-dir)))
+  (yejun/yank-buffer-path nil (+project-root-dir)))
 
 (defun yejun/delete-this-file ()
   (interactive)
