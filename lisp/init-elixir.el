93d0f5c0fa7082d81d65c8cb04480e7558ab467d
Yejun Su <goofan.su@gmail.com>
2023-11-16 10:15:07 +0800

    move elixir functions to elixir-mode use-package block

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -2,16 +2,18 @@
   :pin melpa
   :config
   (message "elixir-mode is loaded")
+
   (with-eval-after-load 'eglot
     (add-to-list 'eglot-server-programs
                  `((elixir-mode elixir-ts-mode heex-ts-mode) .
                    ("nextls" "--stdio=true"))))
+
+  (defun elixir-format-before-save ()
+    (when (derived-mode-p 'elixir-mode)
+      (eglot-format-buffer)))
+
   :hook
   ((elixir-mode elixir-ts-mode heex-ts-mode) . eglot-ensure)
   (before-save . elixir-format-before-save))
-
-(defun elixir-format-before-save ()
-  (when (derived-mode-p 'elixir-mode)
-    (eglot-format-buffer)))
 
 (provide 'init-elixir)
