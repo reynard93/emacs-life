84c517f9f9c9c17562ee10746077f210d6741108
Yejun Su <goofan.su@gmail.com>
2023-11-21 21:30:57 +0800

    make treesit-install-language-grammars always available

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -1,22 +1,21 @@
 (use-package treesit
   :ensure nil
-  :init
+  :preface
   (setq treesit-language-source-alist
         '((ruby "https://github.com/tree-sitter/tree-sitter-ruby")
           (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
           (heex "https://github.com/phoenixframework/tree-sitter-heex")))
 
-  (setq major-mode-remap-alist
-        '((ruby-mode . ruby-ts-mode)))
+  (defun yejun/treesit-install-language-grammars ()
+    "Build and install all tree-sitter language grammar libraries"
+    (interactive)
+    (mapc #'treesit-install-language-grammar
+          (mapcar #'car treesit-language-source-alist)))
 
   :config
   (message "treesit is loaded")
 
-  (defun yejun/treesit-install-language-grammars ()
-    "Build and install all the tree-sitter language grammar libraries
-defined in `treesit-language-source-alist'"
-    (interactive)
-    (mapc #'treesit-install-language-grammar
-          (mapcar #'car treesit-language-source-alist))))
+  :custom
+  (major-mode-remap-alist '((ruby-mode . ruby-ts-mode))))
 
 (provide 'init-tree-sitter)
