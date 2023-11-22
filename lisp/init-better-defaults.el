8e637a98fffa0958a5631a2618e7feb4ba20c8f6
Yejun Su <goofan.su@gmail.com>
2023-11-22 09:47:49 +0800

    disable ring bell

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -1,4 +1,5 @@
 (setq confirm-kill-emacs 'y-or-n-p    ; Confirm when leaving Emacs
+      ring-bell-function 'ignore      ; Disable ring bell
       inhibit-startup-screen t        ; Skip the startup screen
       initial-scratch-message nil     ; Display nothing on the *scratch* buffer
       create-lockfiles nil            ; Avoid creating ".#filename"
