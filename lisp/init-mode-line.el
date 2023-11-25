92c3aad0ff4478a06484881672fe6b30271107b4
Yejun Su <goofan.su@gmail.com>
2023-11-26 02:19:39 +0800

    install anzu and evil-anzu from melpa
    fix https://github.com/emacsorphanage/evil-anzu/pull/6

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -8,14 +8,14 @@
   (doom-modeline-mode 1))
 
 (use-package anzu
-  :pin nongnu
+  :pin melpa
   :defer 1
   :config
   (message "anzu is loaded")
   (global-anzu-mode 1))
 
 (use-package evil-anzu
-  :pin nongnu
+  :pin melpa
   :after (evil anzu)
   :config
   (message "evil-anzu is loaded"))
