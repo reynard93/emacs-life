84619fc2a194f6ae539a9242a2ee8c81243f451f
Yejun Su <goofan.su@gmail.com>
2023-11-30 09:15:36 +0800

    more tree-sitter modes

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -4,7 +4,11 @@
   (setq treesit-language-source-alist
         '((ruby "https://github.com/tree-sitter/tree-sitter-ruby")
           (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
-          (heex "https://github.com/phoenixframework/tree-sitter-heex")))
+          (heex "https://github.com/phoenixframework/tree-sitter-heex")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")))
 
   (defun yejun/treesit-install-language-grammars ()
     "Build and install all tree-sitter language grammar libraries"
@@ -16,6 +20,11 @@
   (message "treesit is loaded")
 
   :custom
-  (major-mode-remap-alist '((ruby-mode . ruby-ts-mode))))
+  (major-mode-remap-alist
+   '((ruby-mode . ruby-ts-mode)
+     (css-mode . css-ts-mode)
+     (javascript-mode . js-ts-mode)
+     (js-json-mode . json-ts-mode)
+     (conf-toml-mode . toml-ts-mode))))
 
 (provide 'init-tree-sitter)
