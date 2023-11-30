7d64377ce9e1c40c71ae1681f4b1b52f4e7f7238
Yejun Su <goofan.su@gmail.com>
2023-11-30 23:47:11 +0800

    add :preface in init-pass

--- lisp/init-pass.el
+++ lisp/init-pass.el
@@ -1,18 +1,19 @@
-(add-to-list 'auth-sources "~/.password-store/.authinfo.gpg")
-
 (use-package pass
   :pin melpa
   :defer t
+  :preface
+  (add-to-list 'auth-sources "~/.password-store/.authinfo.gpg")
+
+  (defun yejun/otp-key-uri (issuer secret)
+    "Create and copy the OTP key URI consisting of issuer and secret."
+    (interactive (list (read-string "Issuer: ")
+                       (read-passwd "Secret: " t)))
+    (let* ((secret (replace-regexp-in-string "\\s-" "" secret))
+           (otp-uri (format "otpauth://totp/totp-secret?secret=%s&issuer=%s" secret issuer)))
+      (kill-new otp-uri)
+      (message "OTP key URI created and copied")))
+
   :config
   (message "pass is loaded"))
-
-(defun yejun/otp-key-uri (issuer secret)
-  "Create and copy the OTP key URI consisting of issuer and secret."
-  (interactive (list (read-string "Issuer: ")
-                     (read-passwd "Secret: " t)))
-  (let* ((secret (replace-regexp-in-string "\\s-" "" secret))
-         (otp-uri (format "otpauth://totp/totp-secret?secret=%s&issuer=%s" secret issuer)))
-    (kill-new otp-uri)
-    (message "OTP key URI created and copied")))
 
 (provide 'init-pass)
