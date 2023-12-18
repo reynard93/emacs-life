01525d1d8ecb04520e050703271c67a12584726a
Yejun Su <goofan.su@gmail.com>
2023-12-18 12:13:02 +0800

    start eglot for ruby projects that are using ruby-lsp

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -1,8 +1,16 @@
 (use-package ruby-ts-mode
   :ensure nil
+  :preface
+  (defun +eglot/start-ruby-lsp ()
+    "Start eglot if a .ruby-lsp directory exists in the project root."
+    (interactive)
+    (let ((ruby-lsp-path (expand-file-name ".ruby-lsp" (+project--root-dir))))
+      (when (file-directory-p ruby-lsp-path)
+        (eglot-ensure))))
+
   :config
   (message "ruby-ts-mode is loaded")
-  :hook (ruby-ts-mode . eglot-ensure))
+  :hook (ruby-ts-mode . +eglot/start-ruby-lsp))
 
 (use-package bundler
   :pin melpa
