e6e0cb34fbeeb70ede996a2f280cc47761c03a7f
Yejun Su <goofan.su@gmail.com>
2023-12-26 14:49:51 +0800

    remove unused keybinding

--- lisp/init-macos-keybindings.el
+++ lisp/init-macos-keybindings.el
@@ -3,7 +3,6 @@
       mac-right-option-modifier 'none)
 
 (bind-key "C-s-f" #'toggle-frame-fullscreen) ; Toggle fullscreen
-(bind-key "s-o" #'find-file)                 ; Open file
 (bind-key "s-s" #'save-buffer)               ; Save file
 (bind-key "s-a" #'mark-whole-buffer)         ; Select all
 (bind-key "s-c" #'kill-ring-save)            ; Copy
