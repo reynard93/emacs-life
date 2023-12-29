d5d75ca10f9d5222f5befa10ec85a3a93d5cc934
Yejun Su <goofan.su@gmail.com>
2023-12-29 11:48:02 +0800

    manage popup buffers with shackle-mode

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -34,28 +34,38 @@
 
 (use-package popper
   :demand t
-  :init
-  (require 'project)
-  (setq popper-group-function #'popper-group-by-project)
-
   :config
   (message "popper is loaded")
   (popper-mode 1)
   (popper-echo-mode 1)
 
   :custom
+  (popper-group-function #'popper-group-by-project)
+  (popper-display-control nil)
   (popper-reference-buffers
-   '("\\*Messages\\*"
-     "\\*Org Select\\*"
-     "^CAPTURE-.*\\.org$"
-     chatgpt-shell-mode
-     compilation-mode
-     osx-dictionary-mode
+   '(compilation-mode
      rspec-compilation-mode
      shell-mode))
 
   :bind (("C-`"   . popper-toggle)
          ("M-`"   . popper-cycle)
          ("C-M-`" . popper-toggle-type)))
+
+(use-package shackle
+  :pin melpa
+  :config
+  (message "shackle is loaded")
+  (shackle-mode 1)
+  :custom
+  (shackle-rules
+   `((magit-status-mode :same t)
+     (osx-dictionary-mode :align below :size 0.3)
+     ((:custom
+       ,(lambda (buffer)
+          (with-current-buffer buffer
+            (bound-and-true-p gptel-mode))))
+      :align right :size 0.5)))
+  (shackle-default-rule
+   '(:align below :size 0.4)))
 
 (provide 'init-windows)
