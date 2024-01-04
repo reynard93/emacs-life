e437a794e8607b037b26dbabfb40cab12e415cc9
Yejun Su <goofan.su@gmail.com>
2024-01-04 15:42:16 +0800

    move logos and olivetti to init-reading

--- modules/init-editing-utils.el
+++ modules/init-editing-utils.el
@@ -36,30 +36,4 @@
   (require 'smartparens-config)
   :hook (prog-mode text-mode))
 
-(use-package logos
-  :init
-  (setq-default logos-hide-cursor nil
-                logos-hide-mode-line t
-                logos-hide-buffer-boundaries t
-                logos-hide-fringe t
-                logos-variable-pitch nil
-                logos-buffer-read-only nil
-                logos-scroll-lock nil
-                logos-olivetti t)
-  :config
-  (message "logos is loaded")
-  :custom
-  (logos-outlines-are-pages t)
-  :bind (([remap narrow-to-region] . logos-narrow-dwim)
-         ([remap forward-page]     . logos-forward-page-dwim)
-         ([remap backward-page]    . logos-backward-page-dwim)))
-
-(use-package olivetti
-  :pin melpa
-  :after logos
-  :config
-  (message "olivetti is loaded")
-  :custom
-  (olivetti-body-width 80))
-
 (provide 'init-editing-utils)
