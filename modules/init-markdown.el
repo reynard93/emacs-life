b9e1dc9d86a1961262f654d0d88f8c0bd4d49967
Yejun Su <goofan.su@gmail.com>
2024-01-06 19:43:38 +0800

    bind <tab> to completion-at-point in markdown-mode insert evil state

--- modules/init-markdown.el
+++ modules/init-markdown.el
@@ -2,6 +2,9 @@
   :pin nongnu
   :defer t
   :config
-  (message "markdown-mode is loaded"))
+  (message "markdown-mode is loaded")
+  (with-eval-after-load 'evil
+    (evil-define-key 'insert markdown-mode-map
+      (kbd "<tab>") #'completion-at-point)))
 
 (provide 'init-markdown)
