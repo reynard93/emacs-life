6027e5f793fce1c9252a8dde8b369bb9dcc74778
Yejun Su <goofan.su@gmail.com>
2024-01-13 22:41:45 +0800

    add work email account

--- modules/init-mail.el
+++ modules/init-mail.el
@@ -9,21 +9,59 @@
 
   :config
   (message "mu4e is loaded")
+  (setq mu4e-context-policy 'pick-first)
+
+  (setq mu4e-contexts
+        (list
+         (make-mu4e-context
+          :name "Personal"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/Personal" (mu4e-message-field msg :maildir))))
+          :vars
+          '((user-mail-address . "goofan.su@gmail.com")
+            (user-full-name    . "Yejun Su")
+            (mu4e-sent-folder  . "/Personal/[Gmail]/Sent Mail")
+            (mu4e-drafts-folder  . "/Personal/[Gmail]/Drafts")
+            (mu4e-trash-folder  . "/Personal/[Gmail]/Trash")
+            (mu4e-refile-folder  . "/Personal/[Gmail]/All Mail")))
+
+         (make-mu4e-context
+          :name "Work"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/Work" (mu4e-message-field msg :maildir))))
+          :vars
+          '((user-mail-address . "james.su@managebac.com")
+            (user-full-name    . "James Su")
+            (mu4e-sent-folder  . "/Work/[Gmail]/Sent Mail")
+            (mu4e-drafts-folder  . "/Work/[Gmail]/Drafts")
+            (mu4e-trash-folder  . "/Work/[Gmail]/Trash")
+            (mu4e-refile-folder  . "/Work/[Gmail]/All Mail")))))
 
   (setq mu4e-maildir-shortcuts
-        '((:maildir "/Inbox"             :key ?i)
-          (:maildir "/[Gmail]/Sent Mail" :key ?s)
-          (:maildir "/[Gmail]/Drafts"    :key ?d)
-          (:maildir "/[Gmail]/Trash"     :key ?t)
-          (:maildir "/[Gmail]/All Mail"  :key ?a)))
+        '(
+          (:maildir "/Personal/Inbox"             :key ?i)
+          (:maildir "/Personal/[Gmail]/Sent Mail" :key ?s)
+          (:maildir "/Personal/[Gmail]/Drafts"    :key ?d)
+          (:maildir "/Personal/[Gmail]/Trash"     :key ?t)
+          (:maildir "/Personal/[Gmail]/All Mail"  :key ?a)
+          (:maildir "/Work/Inbox"                 :key ?I)
+          (:maildir "/Work/[Gmail]/Sent Mail"     :key ?S)
+          (:maildir "/Work/[Gmail]/Drafts"        :key ?D)
+          (:maildir "/Work/[Gmail]/Trash"         :key ?T)
+          (:maildir "/Work/[Gmail]/All Mail"      :key ?A)))
 
   (setq mu4e-bookmarks
         '((:name "Unread messages" :query "flag:unread AND NOT flag:trashed" :key ?u)
           (:name "Today's messages" :query "date:today..now" :key ?t)
           (:name "Last 7 days" :query "date:7d..now" :hide-unread t :key ?w)
-          (:name "Messages with images" :query "mime:image/*" :key ?p)
-          (:name "SourceHut lists" :query "lists.sr.ht" :key ?s)
-          (:name "GitHub notifications" :query "github.com" :key ?g)))
+          (:name "Sourcehut lists" :query "flag:list AND to:lists.sr.ht" :key ?s)
+          (:name "Codeberg lists" :query "flag:list AND from:codeberg.org" :key ?c)
+          (:name "GitHub lists" :query "flag:list AND from:github.com" :key ?g)
+          (:name "Jira" :query "fariaedu.atlassian.net" :key ?j)))
 
   :custom
   (mu4e-update-interval (* 15 60))
