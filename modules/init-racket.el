ccc0c80d0d1296a31c0abadc6bf0618a004c7db7
Yejun Su <goofan.su@gmail.com>
2024-02-04 16:21:26 +0800

    message load

--- modules/init-racket.el
+++ modules/init-racket.el
@@ -1,14 +1,20 @@
 (use-package racket-mode
   :pin nongnu
-  :defer t)
+  :defer t
+  :config
+  (message "racket-mode is loaded"))
 
 (use-package ob-racket
   :ensure nil
   :after racket-mode
-  :load-path "lisp/emacs-ob-racket")
+  :load-path "lisp/emacs-ob-racket"
+  :config
+  (message "ob-racket is loaded"))
 
 (use-package sicp
   :pin melpa
-  :defer t)
+  :defer t
+  :config
+  (message "sicp is loaded"))
 
 (provide 'init-racket)
