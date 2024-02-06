adaf450d877bcbd69eca1e7eb6ca47724af21286
Yejun Su <goofan.su@gmail.com>
2024-02-06 14:05:44 +0800

    remove unused keybinding

--- modules/init-evil-keybindings.el
+++ modules/init-evil-keybindings.el
@@ -58,7 +58,6 @@
     "fD"  #'+file/delete-this-file
     "fe"  #'yejun/browse-emacs-config
     "fn"  #'yejun/browse-nix-config
-    "fp"  #'yejun/open-private-template
     "fo"  #'+macos/reveal-in-finder
     "fO"  #'+macos/reveal-project-in-finder
     "fr"  #'recentf-open-files
