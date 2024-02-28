cedcdb301bf003cfef2fa96af8a09474696f179f
Yejun Su <goofan.su@gmail.com>
2024-02-28 18:05:34 +0800

    use emacs state for sqlite-mode

--- modules/init-data.el
+++ modules/init-data.el
@@ -8,4 +8,10 @@
   :config
   (message "json-mode is loaded"))
 
+(use-package sqlite-mode
+  :defer t
+  :config
+  (message "sqlite-mode is loaded")
+  (evil-set-initial-state 'sqlite-mode 'emacs))
+
 (provide 'init-data)
