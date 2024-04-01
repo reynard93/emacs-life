fec793eedcd1d4054dd0fd39fc5aa41865b97e55
Yejun Su <goofan.su@gmail.com>
2024-04-01 16:27:47 +0800

    add magit-todos

--- modules/init-git.el
+++ modules/init-git.el
@@ -53,4 +53,11 @@
   :config
   (message "git-timemachine is loaded"))
 
+(use-package magit-todos
+  :pin melpa
+  :after magit
+  :config
+  (message "magit-todos is loaded")
+  (magit-todos-mode 1))
+
 (provide 'init-git)
