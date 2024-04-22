846fb3c57862080a4012ddf635466ccd62e41703
Yejun Su <goofan.su@gmail.com>
2024-04-22 17:39:00 +0800

    move embark ace-window actions to lisp-embark.el

--- modules/init-window.el
+++ modules/init-window.el
@@ -9,25 +9,6 @@
   :demand t
   :config
   (message "ace-window is loaded")
-
-  ;; Open any buffer by splitting any window
-  ;; https://karthinks.com/software/fifteen-ways-to-use-embark/#open-any-buffer-by-splitting-any-window
-  (eval-when-compile
-    (defmacro +embark--aw-action (fn)
-      `(defun ,(intern (concat "+embark/aw-" (symbol-name fn))) ()
-         ,(format "Open %s buffer selected with ace-window." (symbol-name fn))
-         (interactive)
-         (with-demoted-errors "%s"
-           (require 'ace-window)
-           (let ((aw-dispatch-always t))
-             (aw-switch-to-window (aw-select nil))
-             (call-interactively (symbol-function ',fn)))))))
-
-  (with-eval-after-load 'embark
-    (keymap-set embark-file-map     "o" (+embark--aw-action find-file))
-    (keymap-set embark-buffer-map   "o" (+embark--aw-action switch-to-buffer))
-    (keymap-set embark-bookmark-map "o" (+embark--aw-action bookmark-jump)))
-
   :custom
   (aw-scope 'frame)
   (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
