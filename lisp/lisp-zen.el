dacf6ee21373a359153eade3974117bd00c7fe23
Yejun Su <goofan.su@gmail.com>
2024-04-30 19:13:34 +0800

    don't change font in +zen/toggle

--- lisp/lisp-zen.el
+++ lisp/lisp-zen.el
@@ -6,11 +6,9 @@
   (if +zen-toggle
       (progn
         (logos-focus-mode -1)
-        (fontaine-set-preset 'regular t)
         (setq-local +zen-toggle nil))
     (progn
       (logos-focus-mode 1)
-      (fontaine-set-preset 'medium t)
       (setq-local +zen-toggle t))))
 
 (provide 'lisp-zen)
