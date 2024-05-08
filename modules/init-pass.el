2ad52fae691144d7dc40afc31ddb4ebb4db9433d
Yejun Su <goofan.su@gmail.com>
2024-05-08 10:02:46 +0800

    embark actions for password-store
    Ref: https://github.com/oantolin/embark/wiki/Additional-Actions#password-store-actions-using-password-storeel

--- modules/init-pass.el
+++ modules/init-pass.el
@@ -3,18 +3,25 @@
   :defer t
   :init
   (auth-source-pass-enable)
+
+  (defvar-keymap embark-password-store-map
+    "c" #'password-store-copy
+    "C" #'password-store-otp-token-copy
+    "f" #'password-store-copy-field
+    "i" #'password-store-insert
+    "I" #'password-store-generate
+    "r" #'password-store-rename
+    "e" #'password-store-edit
+    "k" #'password-store-remove
+    "U" #'password-store-url)
+
+  (with-eval-after-load 'embark
+    (add-to-list 'embark-keymap-alist '(password-store . embark-password-store-map)))
+  (with-eval-after-load 'marginalia
+    (add-to-list 'marginalia-prompt-categories '("Password entry" . password-store)))
+
   :config
   (message "pass is loaded")
-  :bind ( :prefix-map password-store-prefix-map
-          :prefix "C-c p"
-          ("a" . password-store-otp-append)
-          ("A" . password-store-otp-append-from-image)
-          ("e" . password-store-edit)
-          ("i" . password-store-insert)
-          ("I" . password-store-otp-insert)
-          ("K" . password-store-remove)
-          ("p" . password-store-copy)
-          ("r" . password-store-rename)
-          ("t" . password-store-otp-token-copy)))
+  :bind ("C-c p" . password-store-copy))
 
 (provide 'init-pass)
