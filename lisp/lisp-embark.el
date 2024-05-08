cbd021686754075a00c68293b83e81bf04d62efc
Yejun Su <goofan.su@gmail.com>
2024-05-08 11:08:37 +0800

    embark actions for github pull request

--- lisp/lisp-embark.el
+++ lisp/lisp-embark.el
@@ -3,17 +3,17 @@
     (string-match "^#\\([0-9]+\\)" target)
     (match-string 1 target)))
 
-(defun +embark/gh-checkout-pull-request (target)
+(defun +embark/gh-pr-checkout (target)
   "Checkout a pull request with gh command line."
   (when-let ((pr-number (+embark--gh-parse-pr-number target)))
     (shell-command (concat "gh pr checkout " pr-number))))
 
-(defun +embark/gh-browse-pull-request (target)
+(defun +embark/gh-pr-browse (target)
   "Browse a pull request with gh command line."
   (when-let ((pr-number (+embark--gh-parse-pr-number target)))
     (shell-command (concat "gh pr view -w " pr-number))))
 
-(defun +embark/gh-yank-pull-request (target)
+(defun +embark/gh-pr-copy-url (target)
   "Copy a pull request's URL with gh command line."
   (when-let ((pr-number (+embark--gh-parse-pr-number target)))
     (let* ((command (format "gh pr view %s --json url --template '{{.url}}'" pr-number))
@@ -21,10 +21,15 @@
       (kill-new output)
       (message "Copied URL: %s" output))))
 
+(defvar-keymap embark-gh-pr-map
+  "b" #'+embark/gh-pr-browse
+  "c" #'+embark/gh-pr-checkout
+  "U" #'+embark/gh-pr-copy-url)
+
 (with-eval-after-load 'embark
-  (keymap-set embark-general-map "g c" #'+embark/gh-checkout-pull-request)
-  (keymap-set embark-general-map "g b" #'+embark/gh-browse-pull-request)
-  (keymap-set embark-general-map "g y" #'+embark/gh-yank-pull-request))
+  (add-to-list 'embark-keymap-alist '(github-pull-request . embark-gh-pr-map)))
+(with-eval-after-load 'marginalia
+  (add-to-list 'marginalia-prompt-categories '("Select pull request" . github-pull-request)))
 
 ;; Open any buffer by splitting any window
 ;; https://karthinks.com/software/fifteen-ways-to-use-embark/#open-any-buffer-by-splitting-any-window
