8fa20e92a3bcf9769f7a5565c3a9cc3392b2af9a
Yejun Su <goofan.su@gmail.com>
2024-05-24 00:35:35 +0800

    use do-applescript
    Ref: https://irreal.org/blog/?p=4865

--- lisp/lisp-macos.el
+++ lisp/lisp-macos.el
@@ -14,6 +14,6 @@
   (let* ((sound (or sound-name "Default"))
          (script (format "display notification \"%s\" with title \"%s\" sound name \"%s\""
                          body title sound)))
-    (start-process "notifier" nil "osascript" "-e" script)))
+    (do-applescript script)))
 
 (provide 'lisp-macos)
