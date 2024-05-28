81714ade02cbf5e71f78f9e96a42e282953a05a4
Yejun Su <goofan.su@gmail.com>
2024-05-28 12:53:09 +0800

    remove sqlite-mode-extras

--- modules/init-data.el
+++ modules/init-data.el
@@ -14,26 +14,4 @@
   (message "sqlite-mode is loaded")
   (evil-set-initial-state 'sqlite-mode 'emacs))
 
-(use-package sqlite-mode-extras
-  :ensure nil
-  :load-path "vendor/sqlite"
-  :after sqlite-mode
-  :config
-  (message "sqlite-mode-extras is loaded")
-  :bind ( :map sqlite-mode-map
-          ("n" . next-line)
-          ("p" . previous-line)
-          ("b" . sqlite-mode-extras-backtab-dwim)
-          ("f" . sqlite-mode-extras-tab-dwim)
-          ("+" . sqlite-mode-extras-add-row)
-          ("D" . sqlite-mode-extras-delete-row-dwim)
-          ("C" . sqlite-mode-extras-compose-and-execute)
-          ("E" . sqlite-mode-extras-execute)
-          ("S" . sqlite-mode-extras-execute-and-display-select-query)
-          ("DEL" . sqlite-mode-extras-delete-row-dwim)
-          ("g" . sqlite-mode-extras-refresh)
-          ("<backtab>" . sqlite-mode-extras-backtab-dwim)
-          ("<tab>" . sqlite-mode-extras-tab-dwim)
-          ("RET" . sqlite-mode-extras-ret-dwim)))
-
 (provide 'init-data)
