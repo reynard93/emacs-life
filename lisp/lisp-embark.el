d1b9250a15e9fb6953c1aeedcb840bac3c6715ae
Yejun Su <goofan.su@gmail.com>
2024-06-18 16:05:59 +0800

    fix +embark/jira-target-link

--- lisp/lisp-embark.el
+++ lisp/lisp-embark.el
@@ -1,15 +1,13 @@
 (defun +embark/jira-target-link (project)
   "Target a JIRA link at point of the form PROJECT-1234."
   (save-excursion
-    (let* ((start (progn (skip-chars-backward "[:alnum:]-") (point)))
-           (end (progn (skip-chars-forward "[:alnum:]-") (point)))
-           (str (buffer-substring-no-properties start end)))
-      (save-match-data
-        (when (string-match (format "%s-\\([[:alnum:]]+\\)" project) str)
-          `(url
-            ,(format "https://fariaedu.atlassian.net/browse/%s"
-                     (match-string 1 str))
-            ,start . ,end))))))
+    (when-let* ((bounds (bounds-of-thing-at-point 'symbol))
+                (start (car bounds))
+                (end (cdr bounds))
+                (str (buffer-substring-no-properties start end)))
+      (when (string-match (format "%s-\\([[:digit:]]+\\)" project) str)
+        `(url ,(format "https://fariaedu.atlassian.net/browse/%s-%s" project (match-string 1 str))
+              ,start . ,end)))))
 
 (with-eval-after-load 'embark
   (add-to-list 'embark-target-finders (lambda () (+embark/jira-target-link "OA"))))
