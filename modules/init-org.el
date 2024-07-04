4fe756caea3da77ef5fa31a855ee182db9027e0d
Yejun Su <goofan.su@gmail.com>
2024-07-04 13:17:34 +0800

    remove unused templates from org-capture-templates

--- modules/init-org.el
+++ modules/init-org.el
@@ -57,34 +57,16 @@
 
   ;; Capture
   (org-capture-templates
-   `(("t" "Time-sensitive task" entry
-      (file+headline "tasks.org" "Tasks with a date")
-      ,(concat "* TODO %^{Title} %^g\n"
-               "%^{How time sensitive it is|SCHEDULED|DEADLINE}: %^t\n"
-               ":PROPERTIES:\n"
-               ":CAPTURED: %U\n"
-               ":END:\n\n"
-               "%?")
-      :empty-lines-after 1)
-     ("c" "Clock in and do immediately" entry
-      (file+headline "tasks.org" "Clocked tasks")
+   `(("c" "Clock in and do immediately" entry
+      (file "tasks.org")
       ,(concat "* TODO %^{Title}\n"
                ":PROPERTIES:\n"
                ":EFFORT: %^{Effort estimate in minutes|5|10|15|30|45|60|90|120}\n"
-               ":END:\n\n"
-               "%a\n")
+               ":END:\n\n")
       :prepend t
       :clock-in t
       :clock-keep t
       :immediate-finish t
-      :empty-lines-after 1)
-     ("w" "Add to the wishlist (may do some day)" entry
-      (file+headline "tasks.org" "Wishlist")
-      ,(concat "* %^{Title}\n"
-               ":PROPERTIES:\n"
-               ":CAPTURED: %U\n"
-               ":END:\n\n"
-               "%?")
       :empty-lines-after 1)
      ("e" "Email note (unprocessed)" entry
       (file+headline "tasks.org" "Unprocessed")
