41ba7fb43a293b5bc8693e336e72e42b1cf00244
Yejun Su <goofan.su@gmail.com>
2024-07-05 11:40:05 +0800

    update keybinding for Emacs GUI

--- modules/init-gui-frames.el
+++ modules/init-gui-frames.el
@@ -1,12 +1,13 @@
 (when (display-graphic-p)
-  (bind-key "s-x" #'execute-extended-command)  ; M-x
   (bind-key "s-s" #'save-buffer)               ; File -> Save
   (bind-key "s-S" #'write-file)                ; File -> Save As
   (bind-key "s-o" #'find-file)                 ; File -> Open File
   (bind-key "s-a" #'mark-whole-buffer)         ; File -> Select All
   (bind-key "s-c" #'kill-ring-save)            ; Edit -> Copy
   (bind-key "s-v" #'yank)                      ; Edit -> Paste
-  (bind-key "s-z" #'undo))                     ; Edit -> Undo
+  (bind-key "s-x" #'kill-region)               ; Edit -> Cut
+  (bind-key "s-z" #'undo)                      ; Edit -> Undo
+  (bind-key "s-Z" #'undo-redo))                ; Edit -> Redo
 
 (use-package tab-bar
   :ensure nil
