6c6cd9ac2bd95cce31a41a77ffc029f30dff5d3d
Yejun Su <goofan.su@gmail.com>
2024-07-06 12:55:45 +0800

    remove unused functions

--- lisp/lisp-project.el
+++ lisp/lisp-project.el
@@ -8,18 +8,4 @@
     (unless project (user-error "Not in a project"))
     (project-root project)))
 
-(defun +project/search ()
-  (interactive)
-  (let ((dir (+project/root-dir)))
-    (funcall-interactively #'consult-ripgrep dir)))
-
-(defun +project/search-for-symbol-at-point ()
-  (interactive)
-  (let ((dir (+project/root-dir))
-        (initial (thing-at-point 'symbol t)))
-    (funcall-interactively #'consult-ripgrep dir initial)))
-
-(bind-key "s" #'+project/search 'search-map)
-(bind-key "S" #'+project/search-for-symbol-at-point 'search-map)
-
 (provide 'lisp-project)
