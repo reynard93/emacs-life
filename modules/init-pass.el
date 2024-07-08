0602a20d72ee51af4e4ca982bbf756a803dc0e63
Yejun Su <goofan.su@gmail.com>
2024-07-08 10:09:10 +0800

    add +pass/kill-ring-save-otp-uri

--- modules/init-pass.el
+++ modules/init-pass.el
@@ -1,6 +1,15 @@
 (use-package pass
   :pin melpa
   :defer t
+  :preface
+  (defun +pass/kill-ring-save-otp-uri (issuer secret)
+    "Create and copy the OTP URI to kill ring consisting of issuer and secret."
+    (interactive (list (read-string "Issuer: ")
+                       (read-passwd "Secret: " t)))
+    (let* ((secret (replace-regexp-in-string "\\s-" "" secret))
+           (otp-uri (format "otpauth://totp/totp-secret?secret=%s&issuer=%s" secret issuer)))
+      (kill-new otp-uri)
+      (message "OTP URI is created and copied to kill ring")))
   :init
   (auth-source-pass-enable)
 
