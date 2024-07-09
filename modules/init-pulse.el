b78cc8cce2a8a767f25f570e32c4625e15c658a7
Yejun Su <goofan.su@gmail.com>
2024-07-09 17:18:19 +0800

    simplify pulse configuration

--- modules/init-pulse.el
+++ modules/init-pulse.el
@@ -1,26 +1,13 @@
 (use-package pulsar
   :config
   (message "pulsar is loaded")
-  (pulsar-global-mode 1)
-  (add-hook 'fontaine-set-preset-hook #'pulsar-pulse-line)
-  :custom
-  (pulsar-face 'pulsar-magenta)
-  (pulsar-pulse-functions
-   '(ace-window
-     delete-window
-     delete-other-windows
-     tab-new
-     tab-close
-     tab-next
-     tab-previous
-     widen)))
+  (pulsar-global-mode 1))
 
 (use-package goggles
   :pin melpa
-  :init
-  (setq-default goggles-pulse t)
   :config
   (message "goggles is loaded")
+  (setq-default goggles-pulse t)
   :hook (prog-mode text-mode))
 
 (provide 'init-pulse)
