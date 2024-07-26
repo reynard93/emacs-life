60c02fa5f9e5a89d6bdf70ca247dd9645dfff714
Yejun Su <goofan.su@gmail.com>
2024-07-26 23:02:25 +0800

    refactor init-completion.el

--- modules/init-completion.el
+++ modules/init-completion.el
@@ -1,10 +1,5 @@
 (use-package corfu
-  :preface
-  (setq tab-always-indent 'complete     ; Enable indentation+completion using the TAB key
-        completion-cycle-threshold 3)   ; TAB cycle if there are only few candidates
-
-  :config
-  (message "corfu is loaded")
+  :init
   (global-corfu-mode 1)
   :custom
   (corfu-cycle t)
@@ -12,34 +7,29 @@
   (corfu-quit-no-match t)
   (corfu-scroll-margin 5))
 
+(use-package emacs
+  :custom
+  ;; Enable indentation+completion using the TAB key.
+  (tab-always-indent 'complete)
+  ;; TAB cycle if there are only few candidates
+  (completion-cycle-threshold 3))
+
 (use-package cape
   :init
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
   (add-to-list 'completion-at-point-functions #'cape-history)
-  (add-to-list 'completion-at-point-functions #'cape-keyword)
-  :config
-  (message "cape is loaded"))
+  (add-to-list 'completion-at-point-functions #'cape-keyword))
 
 (use-package tempel
   :init
-  (defun +tempel/find-private-template ()
-    (interactive)
-    (let ((file (expand-file-name "templates/private.eld" user-emacs-directory)))
-      (find-file file)))
-
-  :config
-  (message "tempel is loaded")
-  (defun +tempel--setup-capf ()
+  (setq tempel-path (expand-file-name "templates/*.eld" user-emacs-directory))
+  (defun tempel-setup-capf ()
     (setq-local completion-at-point-functions
                 (cons #'tempel-expand
                       completion-at-point-functions)))
-
-  :custom
-  (tempel-path (expand-file-name "templates/*.eld" user-emacs-directory))
-  :hook
-  ((prog-mode text-mode) . +tempel--setup-capf)
+  :hook ((prog-mode text-mode) . tempel-setup-capf)
   :bind (("M-+" . tempel-complete)
          ("M-*" . tempel-insert)))
 
