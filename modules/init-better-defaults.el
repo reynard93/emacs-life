efa85aa7a032ddc40303f6c87c85756b94f45249
Yejun Su <goofan.su@gmail.com>
2024-07-27 00:09:07 +0800

    refactor init-editing-utils.el

--- modules/init-better-defaults.el
+++ modules/init-better-defaults.el
@@ -1,135 +1,13 @@
-(setq inhibit-startup-screen t        ; Skip the startup screen
-      initial-scratch-message nil     ; Blank the *scratch* buffer
-      use-dialog-box nil              ; Disable dialog windows
-      confirm-kill-emacs 'y-or-n-p    ; Confirm when leaving Emacs
-      ring-bell-function 'ignore      ; Disable ring bell
-      create-lockfiles nil            ; Avoid creating ".#filename"
-      make-backup-files nil)          ; Avoid creating "filename~"
+(recentf-mode 1)
+(save-place-mode 1)
+(blink-cursor-mode -1)
+(global-subword-mode 1)
 
-(setq native-comp-async-report-warnings-errors 'silent)
-(setq initial-major-mode 'fundamental-mode)
-(setq ns-use-native-fullscreen nil)
-
-(use-package recentf
-  :ensure nil
-  :config
-  (message "recentf is loaded")
-  (recentf-mode 1))
-
-(use-package savehist
-  :ensure nil
-  :config
-  (message "savehist is loaded")
-  (add-to-list 'savehist-additional-variables 'log-edit-comment-ring)
-  (savehist-mode 1)
-  :custom
-  (history-length 500)
-  (history-delete-duplicates t))
-
-(use-package ibuffer
-  :ensure nil
-  :config
-  (message "ibuffer is loaded")
-  :custom
-  (ibuffer-expert t)
-  :hook (ibuffer-mode . ibuffer-auto-mode)
-  :bind ([remap list-buffers] . ibuffer))
-
-(use-package ediff
-  :ensure nil
-  :defer t
-  :config
-  (message "ediff is loaded")
-  :custom
-  (ediff-window-setup-function #'ediff-setup-windows-plain)
-  (ediff-split-window-function #'split-window-horizontally))
-
-(use-package helpful
-  :pin melpa
-  :config
-  (message "helpful is loaded")
-  :bind (("C-h f" . helpful-callable)
-         ("C-h v" . helpful-variable)
-         ("C-h k" . helpful-key)
-         ("C-h x" . helpful-command)))
-
-(use-package undo-fu
-  :pin melpa
-  :init
-  (setq undo-limit 67108864          ; 64mb
-        undo-strong-limit 100663296  ; 96mb
-        undo-outer-limit 1006632960) ; 960mb
-  :config
-  (message "undo-fu is loaded"))
-
-(use-package undo-fu-session
-  :pin melpa
-  :after undo-fu
-  :config
-  (message "undo-fu-session is loaded")
-  (undo-fu-session-global-mode 1)
-  :custom
-  (undo-fu-session-compression 'zst)
-  (undo-fu-session-incompatible-files '("/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'")))
-
-(use-package xclip
-  :unless (display-graphic-p)
-  :config
-  (message "xclip is loaded")
-  (xclip-mode 1))
-
-(use-package compile
-  :ensure nil
-  :config
-  (message "compile is loaded")
-  (defun compilation-filter-colorize ()
-    (let ((inhibit-read-only t))
-      (ansi-color-apply-on-region compilation-filter-start (point-max))))
-  :hook (compilation-filter . compilation-filter-colorize))
-
-(use-package project
-  :ensure nil
-  :defer t
-  :config
-  (message "project is loaded")
-  :custom
-  (project-switch-commands
-   '((project-find-file "Find file")
-     (project-find-dir "Find directory")
-     (project-dired "Root dired")
-     (project-eshell "Eshell"))))
-
-(use-package time
-  :ensure nil
-  :custom
-  (world-clock-time-format "%F %T %z")
-  (zoneinfo-style-world-list
-   '(("America/Vancouver" "Vancouver")
-     ("America/Chicago" "Portland")
-     ("UTC" "UTC")
-     ("Europe/London" "London")
-     ("Europe/Kyiv" "Kyiv")
-     ("Asia/Shanghai" "Shanghai"))))
-
-(use-package crux
-  :pin melpa
-  :config
-  (message "crux is loaded")
-  :bind (("C-c d" . crux-duplicate-current-line-or-region)
-         ("C-c M-d" . crux-duplicate-and-comment-current-line-or-region)
-         ("s-k" . crux-smart-kill-line)
-         ("s-j" . crux-top-join-line)
-         ("s-o" . crux-smart-open-line-above)
-         ("M-o" . crux-smart-open-line)
-         ("C-^" . crux-switch-to-previous-buffer)
-         ("s-n" . crux-create-scratch-buffer)
-         ([remap move-beginning-of-line] . crux-move-beginning-of-line)
-         ([remap kill-whole-line] . crux-kill-whole-line)))
-
-;; https://tecosaur.github.io/emacs-config/config.html#better-defaults
 (setq-default delete-by-moving-to-trash t         ; Delete files to trash
               window-combination-resize t         ; take new window space from all other windows (not just current)
-              x-stretch-cursor t)                 ; Stretch cursor to the glyph width
+              x-stretch-cursor t                  ; Stretch cursor to the glyph width
+              indent-tabs-mode nil
+              fill-column 80)
 
 (setq auto-save-default t                         ; Nobody likes to loose work, I certainly don't
       truncate-string-ellipsis "…"               ; Unicode ellispis are nicer than "...", and also save /precious/ space
@@ -137,9 +15,60 @@
       scroll-margin 2                             ; It's nice to maintain a little margin
       display-time-default-load-average nil       ; I don't think I've ever found this useful
       use-short-answers t                         ; Prefer short anwsers
-      require-final-newline t)                    ; Save file with a trailing newline
+      require-final-newline t                     ; Save file with a trailing newline
+      inhibit-startup-screen t                    ; Skip the startup screen
+      initial-scratch-message nil                 ; Blank the *scratch* buffer
+      use-dialog-box nil                          ; Disable dialog windows
+      confirm-kill-emacs 'y-or-n-p                ; Confirm when leaving Emacs
+      ring-bell-function 'ignore                  ; Disable ring bell
+      create-lockfiles nil                        ; Avoid creating ".#filename"
+      make-backup-files nil)                      ; Avoid creating "filename~"
 
-(add-to-list 'default-frame-alist '(height . 24))
-(add-to-list 'default-frame-alist '(width . 80))
+(setq ns-use-native-fullscreen nil
+      native-comp-async-report-warnings-errors 'silent
+      initial-major-mode 'fundamental-mode)
+
+(use-package savehist
+  :init
+  (savehist-mode 1)
+  :custom
+  (history-length 500)
+  (history-delete-duplicates t))
+
+(use-package ibuffer
+  :hook (ibuffer-mode . ibuffer-auto-mode)
+  :bind ([remap list-buffers] . ibuffer))
+
+(use-package ediff
+  :custom
+  (ediff-window-setup-function #'ediff-setup-windows-plain)
+  (ediff-split-window-function #'split-window-horizontally))
+
+(use-package compile
+  :init
+  (defun compilation-filter-colorize ()
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  :hook (compilation-filter . compilation-filter-colorize))
+
+(use-package helpful
+  :pin melpa
+  :bind (("C-h f" . helpful-callable)
+         ("C-h v" . helpful-variable)
+         ("C-h k" . helpful-key)
+         ("C-h x" . helpful-command)))
+
+(use-package crux
+  :pin melpa
+  :bind (([remap move-beginning-of-line] . crux-move-beginning-of-line)
+         ([remap kill-whole-line] . crux-kill-whole-line)
+         ("C-c d" . crux-duplicate-current-line-or-region)
+         ("C-c M-d" . crux-duplicate-and-comment-current-line-or-region)
+         ("s-k" . crux-smart-kill-line)
+         ("s-j" . crux-top-join-line)
+         ("s-o" . crux-smart-open-line-above)
+         ("M-o" . crux-smart-open-line)
+         ("C-^" . crux-switch-to-previous-buffer)
+         ("s-n" . crux-create-scratch-buffer)))
 
 (provide 'init-better-defaults)
--- modules/init-editing-utils.el
+++ modules/init-editing-utils.el
@@ -1,66 +1,57 @@
-(blink-cursor-mode -1)
-(global-subword-mode 1)
-(save-place-mode 1)
+(use-package undo-fu
+  :pin melpa
+  :init
+  (setq undo-limit 67108864             ; 64mb
+        undo-strong-limit 100663296     ; 96mb
+        undo-outer-limit 1006632960))   ; 960mb
 
-(setq-default indent-tabs-mode nil
-              fill-column 80)
+(use-package undo-fu-session
+  :pin melpa
+  :after undo-fu
+  :init
+  (undo-fu-session-global-mode 1)
+  :custom
+  (undo-fu-session-compression 'zst))
 
 (use-package apheleia
   :pin melpa
-  :defer t
-  :config
-  (message "apheleia is loaded")
   :bind ("C-c f" . apheleia-format-buffer))
 
 (use-package avy
-  :config
-  (message "avy is loaded")
   :bind ( :map goto-map
           ("s" . avy-goto-char-2)))
 
 (use-package rainbow-delimiters
   :pin nongnu
-  :config
-  (message "rainbow-delimiters is loaded")
   :hook prog-mode)
 
 (use-package smartparens
   :pin melpa
   :config
-  (message "smartparens is loaded")
   (require 'smartparens-config)
   :hook (prog-mode text-mode))
 
-(use-package wgrep
-  :pin nongnu
-  :defer t
-  :config
-  (message "wgrep is loaded"))
-
 (use-package substitute
-  :config
-  (message "substitute is loaded")
   :bind-keymap ("C-c s" . substitute-prefix-map))
 
 (use-package evil-nerd-commenter
   :pin nongnu
-  :defer t
-  :config
-  (message "evil-nerd-commenter is loaded")
   :bind ("s-;" . evilnc-comment-or-uncomment-lines))
 
 (use-package multiple-cursors
   :pin nongnu
-  :config
-  (message "multiple-cursors is loaded")
   :bind (("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)))
 
 (use-package move-text
   :pin melpa
-  :config
-  (message "move-text is loaded")
   :bind (("s-<up>" . move-text-up)
          ("s-<down>" . move-text-down)))
+
+(use-package wgrep
+  :pin nongnu
+  :defer t
+  :custom
+  (wgrep-auto-save-buffer t))
 
 (provide 'init-editing-utils)
