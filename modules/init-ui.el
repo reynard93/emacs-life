49d1bc6380a99c8c6fc1198c32fc404ad7f4ba44
Yejun Su <goofan.su@gmail.com>
2024-07-27 00:54:38 +0800

    refactor init-ui.el and init-ux.el

--- modules/init-ui.el
+++ modules/init-ui.el
@@ -1,50 +1,15 @@
 (use-package modus-themes
   :demand t
   :config
-  (message "modus-themes is loaded")
-  (modus-themes-load-theme
-   (if (display-graphic-p)
-       (if (+macos/dark-mode-p)
-           'modus-vivendi
-         'modus-operandi)
-     'modus-vivendi))
+  (load-theme 'modus-vivendi :no-confirm)
   :custom
   (modus-themes-mixed-fonts t)
-  (modus-themes-variable-pitch-ui t))
-
-(use-package ef-themes
-  :defer t
-  :config
-  (message "ef-themes is loaded")
-  :custom
-  (ef-themes-mixed-fonts t)
-  (ef-themes-variable-pitch-ui t))
-
-(use-package theme-buffet
-  :if (display-graphic-p)
-  :after (:any modus-themes ef-themes)
-  :demand t
-  :config
-  (message "theme-buffet is loaded")
-  (theme-buffet-timer-hours 1)
-  (defun +theme-buffet/toggle ()
-    (interactive)
-    (if theme-buffet-timer-hours
-        (theme-buffet--free-timer 'theme-buffet-timer-hours)
-      (theme-buffet-timer-hours 1)))
-  :custom
-  (theme-buffet-menu 'end-user)
-  (theme-buffet-end-user
-   '( :night     (modus-vivendi ef-dark ef-winter ef-autumn ef-night ef-duo-dark ef-symbiosis)
-      :morning   (modus-operandi ef-light ef-cyprus ef-spring ef-frost ef-duo-light)
-      :afternoon (modus-operandi-tinted ef-arbutus ef-day ef-kassio ef-summer ef-elea-light ef-maris-light ef-melissa-light ef-trio-light ef-reverie)
-      :evening   (modus-vivendi-tinted ef-rosa ef-elea-dark ef-maris-dark ef-melissa-dark ef-trio-dark ef-dream))))
+  (modus-themes-variable-pitch-ui t)
+  :bind ("<f5>" . modus-themes-toggle))
 
 (use-package fontaine
-  :if (display-graphic-p)
   :demand t
   :config
-  (message "fontaine is loaded")
   (fontaine-set-preset 'regular)
   :custom
   (fontaine-presets
@@ -62,7 +27,6 @@
       :variable-pitch-weight nil
       :variable-pitch-height 1.0)
      ))
-  :hook (modus-themes-after-load-theme . fontaine-apply-current-preset)
   :bind ("C-c F" . fontaine-set-preset))
 
 (provide 'init-ui)
--- modules/init-ux.el
+++ modules/init-ux.el
@@ -1,21 +1,11 @@
-(use-package visual-line-mode
-  :ensure nil
-  :config
-  (message "visual-line-mode is loaded")
-  :bind ("<f5>" . visual-line-mode)
-  :hook (org-mode markdown-mode gptel-mode))
-
 (use-package whitespace
   :ensure nil
-  :config
-  (message "whitespace is loaded")
   :bind (("<f6>" . whitespace-mode)
          ("C-c z" . delete-trailing-whitespace)))
 
 (use-package display-line-numbers
   :ensure nil
   :config
-  (message "display-lines-numbers is loaded")
   (setq-default display-line-numbers-type t)
   (setq-default display-line-numbers-widen t)
   :custom
@@ -24,17 +14,11 @@
   :bind ("<f7>" . display-line-numbers-mode))
 
 (use-package spacious-padding
-  :if (display-graphic-p)
-  :demand t
-  :config
-  (message "spacious-padding is loaded")
-  (spacious-padding-mode 1)
   :custom
   (spacious-padding-subtle-mode-line t)
   :bind ("<f8>" . spacious-padding-mode))
 
 (use-package logos
-  :if (display-graphic-p)
   :init
   (setq-default logos-hide-cursor nil
                 logos-hide-mode-line t
@@ -44,8 +28,6 @@
                 logos-buffer-read-only nil
                 logos-scroll-lock nil
                 logos-olivetti t)
-  :config
-  (message "logos is loaded")
   :custom
   (logos-outlines-are-pages t)
   :bind (([remap narrow-to-region] . logos-narrow-dwim)
@@ -57,24 +39,9 @@
 
 (use-package olivetti
   :pin melpa
-  :defer t
-  :config
-  (message "olivetti is loaded")
   :custom
   (olivetti-body-width 0.7)
   (olivetti-minimum-body-width 80)
   (olivetti-recall-visual-line-mode-entry-state t))
-
-(use-package outline
-  :ensure nil
-  :config
-  (message "outline is loaded")
-  :custom
-  (outline-minor-mode-highlight nil)
-  (outline-minor-mode-cycle t)
-  (outline-minor-mode-use-buttons nil)
-  (outline-minor-mode-use-margins nil)
-  :bind
-  ("<f10>" . outline-minor-mode))
 
 (provide 'init-ux)
