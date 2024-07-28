e3fa2546de45dea4b0050e4b8b62b49ce3c79fd2
Yejun Su <goofan.su@gmail.com>
2024-07-28 13:33:01 +0800

    remove elixir from eglot-server-programs

--- modules/init-eglot.el
+++ modules/init-eglot.el
@@ -1,25 +1,11 @@
 (use-package eglot
   :ensure nil
-  :defer t
-  :init
-  (defun +eglot/start-ruby-lsp ()
-    "Start eglot if a .ruby-lsp directory exists in the project root."
-    (interactive)
-    (let ((ruby-lsp-path
-           (condition-case nil
-               (expand-file-name ".ruby-lsp" (project-root (project-current)))
-             (error nil))))
-      (when (and ruby-lsp-path (file-directory-p ruby-lsp-path))
-        (setq-local eglot-ignored-server-capabilities '(:hoverProvider :completionProvider))
-        (eglot-ensure))))
   :config
-  (dolist (mode '(((elixir-ts-mode heex-ts-mode) . ("nextls" "--stdio=true"))
-                  (ruby-ts-mode . ("ruby-lsp"))
+  (dolist (mode '((ruby-ts-mode . ("ruby-lsp"))
                   (nix-mode . ("nixd"))))
     (add-to-list 'eglot-server-programs mode))
   :hook
-  ((elixir-ts-mode heex-ts-mode) . eglot-ensure)
-  (ruby-ts-mode . +eglot/start-ruby-lsp)
+  (ruby-ts-mode . eglot-ensure)
   (nix-mode . eglot-ensure))
 
 (provide 'init-eglot)
--- modules/init-elixir.el
+++ modules/init-elixir.el
@@ -8,7 +8,7 @@
   :config
   (defun elixir-format-before-save ()
     (when (derived-mode-p 'elixir-ts-mode)
-      (eglot-format-buffer)))
+      (apheleia-format-buffer 'mix-format)))
   :hook (before-save . elixir-format-before-save))
 
 (provide 'init-elixir)
