852f7c4c9b8b7f4c644bc97dcba46ec58008f9b2
Yejun Su <goofan.su@gmail.com>
2024-07-28 18:17:11 +0800

    enable goggles also for conf-mode

--- modules/init-ux.el
+++ modules/init-ux.el
@@ -54,7 +54,7 @@
   :pin melpa
   :config
   (setq-default goggles-pulse t)
-  :hook (prog-mode text-mode))
+  :hook (prog-mode text-mode conf-mode))
 
 (use-package rainbow-delimiters
   :pin nongnu
