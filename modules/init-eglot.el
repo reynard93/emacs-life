93d01e6f53167b8f42351dcc68e43c69fdabbfec
Yejun Su <goofan.su@gmail.com>
2024-07-31 02:49:49 +0800

    ignore Eglot's completion capability
    To use tempel templates

--- modules/init-eglot.el
+++ modules/init-eglot.el
@@ -1,8 +1,8 @@
 (use-package eglot
   :ensure nil
-  :hook
-  (ruby-ts-mode . eglot-ensure)
-  (nix-mode . eglot-ensure)
+  :hook ((ruby-ts-mode nix-mode) . eglot-ensure)
+  :custom
+  (eglot-ignored-server-capabilities '(:completionProvider))
   :config
   (dolist (mode '((ruby-ts-mode . ("ruby-lsp"))
                   (nix-mode . ("nixd"))))
