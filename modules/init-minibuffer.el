093bc52aea7a837ebc61ac85c5a20fb4aa6f9090
Yejun Su <goofan.su@gmail.com>
2024-09-26 11:49:28 +0800

    add suggested options for consult-{register, xref}

--- modules/init-minibuffer.el
+++ modules/init-minibuffer.el
@@ -66,6 +66,21 @@
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package consult
+  :init
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
   :bind
   (([remap goto-line] . consult-goto-line)
    ([remap bookmark-jump] . consult-bookmark)
