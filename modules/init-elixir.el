3bdb4b973d7b69fa99853c8ce4efc16e2f30ce9c
Yejun Su <goofan.su@gmail.com>
2024-10-09 15:10:31 +0800

    move +elixir/package-config to :config

--- modules/init-elixir.el
+++ modules/init-elixir.el
@@ -1,22 +1,22 @@
-(use-package heex-ts-mode
-  :pin melpa
-  :defer t)
-
 (use-package elixir-ts-mode
   :pin melpa
-  :init
-  (defun +elixir/package-config (package)
-    "Fetch PACKAGE's mix.exs config and insert at point."
-    (interactive "sPackage: ")
-    (let* ((command (format "mix hex.info %s | grep 'Config:' | sed 's/Config: //g'" package))
-           (result (shell-command-to-string command)))
-      (insert result)))
   :hook (before-save . elixir-format-before-save)
   :bind (:map my-insert-map ("e e" . +elixir/package-config))
   :config
   (defun elixir-format-before-save ()
     (when (derived-mode-p 'elixir-ts-mode)
-      (eglot-format-buffer))))
+      (eglot-format-buffer)))
+
+  (defun +elixir/package-config (package)
+    "Fetch PACKAGE's mix.exs config and insert at point."
+    (interactive "sPackage: ")
+    (let* ((command (format "mix hex.info %s | grep 'Config:' | sed 's/Config: //g'" package))
+           (result (shell-command-to-string command)))
+      (insert result))))
+
+(use-package heex-ts-mode
+  :pin melpa
+  :defer t)
 
 (use-package exunit
   :pin melpa
