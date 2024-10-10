4a5c594afce9602605c695847531ab3bbe98718f
Yejun Su <goofan.su@gmail.com>
2024-10-10 11:58:40 +0800

    Revert "use Libera Chat directly"
    This reverts commit b6c46268574216af43d69c9aa0f056aeaa9e96eb.

--- modules/init-irc.el
+++ modules/init-irc.el
@@ -2,22 +2,30 @@
   :pin melpa
   :init
   (setq circe-network-defaults
-        '(("Libera.Chat"
-           :host "irc.libera.chat"
+        '(("chat.sr.ht/Libera.Chat"
+           :host "chat.sr.ht"
            :port 6697
            :use-tls t
            :nick "goofansu"
            :realname "Yejun Su"
-           :sasl-username "goofansu"
-           :sasl-password (lambda (&rest _) (auth-source-pass-get 'secret "irc.libera.chat"))
-           :channels ("#emacs" "#nixos" "#ruby" "#elixir"))))
+           :sasl-username "goofansu/irc.libera.chat"
+           :sasl-password (lambda (&rest _) (auth-source-pass-get 'secret "chat.sr.ht"))
+           :nickserv-password (lambda (&rest _) (auth-source-pass-get 'secret "irc.libera.chat")))))
 
   :bind ( :map goto-map
-          ("K" . +circe/jump-to-channel))
+          ("K" . +circe/jump-to-channel)
+          :map circe-channel-mode-map
+          ("C-c C-p" . +circe/pull-recent-messages))
 
   :config
   (circe-set-display-handler "353" 'circe-display-ignore)
   (circe-set-display-handler "366" 'circe-display-ignore)
+
+  (defun +circe/pull-recent-messages (limit)
+    "Request up to LIMIT number of the most recent messages that have been sent."
+    (interactive "sEnter the number of the most recent messages to request: ")
+    (circe-command-QUOTE
+     (format "CHATHISTORY LATEST %s * %s" circe-chat-target limit)))
 
   (defun +circe/jump-to-channel ()
     (interactive)
