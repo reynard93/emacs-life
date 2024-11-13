73f8af50e1d4f09809536092e4b6446d8caafdc3
Yejun Su <goofan.su@gmail.com>
2024-11-13 16:32:35 +0800

    remove my-assistant-map

--- modules/init-keymaps.el
+++ modules/init-keymaps.el
@@ -3,9 +3,4 @@
 
 (define-key global-map (kbd "C-c i") my-insert-map)
 
-(defvar my-assistant-map (make-sparse-keymap)
-  "Keymap for assistant commands")
-
-(define-key global-map (kbd "C-c a") my-assistant-map)
-
 (provide 'init-keymaps)
