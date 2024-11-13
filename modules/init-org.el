8f9da2e96349c9cfb9e1dafcf533a1f206aacb19
Yejun Su <goofan.su@gmail.com>
2024-11-13 17:21:22 +0800

    add back org-agenda settings

--- modules/init-org.el
+++ modules/init-org.el
@@ -1,31 +1,47 @@
 (use-package org
   :ensure nil
   :bind
-  (("C-c l" . org-store-link)
+  (("C-c a" . org-agenda)
    ("C-c c" . org-capture)
+   ("C-c l" . org-store-link)
    :map org-mode-map
    ([remap mark-defun] . org-babel-mark-block)
    ("M-g o" . consult-org-heading))
 
   :custom
-  ;; Display
   (org-ellipsis "…")
-  (org-use-sub-superscripts '{})
+  (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
 
-  ;; Movement
-  (org-special-ctrl-a/e t)
-  (org-special-ctrl-k t)
+  ;; Agenda
+  (org-agenda-files `(,org-directory))
+  (org-agenda-window-setup 'current-window)
+
+  ;; Capture
+  (org-capture-templates
+   `(("t" "Task" entry
+      (file "tasks.org")
+      ,(concat "* TODO %?\n"
+               ":PROPERTIES:\n"
+               ":CAPTURED: %U\n"
+               ":END:\n\n"
+               "%a\n%i")
+      :empty-lines-after 1)))
 
   ;; Code block
+  (org-edit-src-content-indentation 0)
+  (org-edit-src-persistent-message nil)
   (org-src-preserve-indentation t)
+  (org-src-window-setup 'current-window)
 
-  ;; Export
-  (org-export-dispatch-use-expert-ui t)
-  (org-export-with-sub-superscripts '{})
-
-  ;; Logging
+  ;; Log
   (org-log-done 'time)
   (org-log-into-drawer t)
+  (org-log-redeadline 'time)
+  (org-log-reschedule 'time)
+
+  ;; Tag
+  (org-auto-align-tags nil)
+  (org-tags-column 0)
 
   :config
   (org-babel-do-load-languages
