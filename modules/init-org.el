16fccbbe9376ae3f18ca56f67590aa7e88d93b8a
Yejun Su <goofan.su@gmail.com>
2024-11-15 10:42:34 +0800

    remove org-agenda and org-capture-templates

--- modules/init-org.el
+++ modules/init-org.el
@@ -1,8 +1,7 @@
 (use-package org
   :ensure nil
   :bind
-  (("C-c a" . org-agenda)
-   ("C-c c" . org-capture)
+  (("C-c c" . org-capture)
    ("C-c l" . org-store-link)
    :map org-mode-map
    ([remap mark-defun] . org-babel-mark-block)
@@ -12,34 +11,11 @@
   (org-ellipsis "…")
   (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
 
-  ;; Agenda
-  (org-agenda-files `(,org-directory))
-  (org-agenda-window-setup 'current-window)
-
-  ;; Capture
-  (org-capture-templates
-   `(("t" "Task" entry
-      (file "tasks.org")
-      ,(concat "* TODO %^{Title}\n"
-               ":PROPERTIES:\n"
-               ":CAPTURED: %U\n"
-               ":END:\n\n"
-               "%a\n")
-      :prepend t
-      :immediate-finish t
-      :empty-lines-after 1)))
-
   ;; Code block
   (org-edit-src-content-indentation 0)
   (org-edit-src-persistent-message nil)
   (org-src-preserve-indentation t)
   (org-src-window-setup 'current-window)
-
-  ;; Log
-  (org-log-done 'time)
-  (org-log-into-drawer t)
-  (org-log-redeadline 'time)
-  (org-log-reschedule 'time)
 
   ;; Tag
   (org-auto-align-tags nil)
