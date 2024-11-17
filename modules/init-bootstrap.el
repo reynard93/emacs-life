699da2f760bff77a04e7fb2dbdb07d8a8efb66e5
Yejun Su <goofan.su@gmail.com>
2024-11-17 22:57:42 +0800

    load exec-path-from-shell as early as possible
    To set doc-view-pdfdraw-program correctly

--- modules/init-bootstrap.el
+++ modules/init-bootstrap.el
@@ -14,6 +14,18 @@
   (package-vc-install "https://github.com/slotThe/vc-use-package"))
 (require 'vc-use-package)
 
+;; Set $PATH correctly
+(use-package exec-path-from-shell
+  :pin nongnu
+  :if (memq window-system '(mac ns x))
+  :config
+  (exec-path-from-shell-initialize)
+  ;; Add Homebrew's executable files to PATH.
+  (let ((homebrew-bin-dir "/opt/homebrew/bin"))
+    (when (and (file-directory-p homebrew-bin-dir)
+               (not (string-match-p homebrew-bin-dir (getenv "PATH"))))
+      (setenv "PATH" (concat (getenv "PATH") ":" homebrew-bin-dir)))))
+
 ;; Enabling `envrc-global-mode'
 (use-package envrc
   :pin melpa
--- modules/init-macos.el
+++ modules/init-macos.el
@@ -16,15 +16,4 @@
    :map embark-url-map
    ("U" . macos-read-it-later)))
 
-(use-package exec-path-from-shell
-  :pin nongnu
-  :if (memq window-system '(mac ns x))
-  :config
-  (exec-path-from-shell-initialize)
-  ;; Add Homebrew's executable files to PATH.
-  (let ((homebrew-bin-dir "/opt/homebrew/bin"))
-    (when (and (file-directory-p homebrew-bin-dir)
-               (not (string-match-p homebrew-bin-dir (getenv "PATH"))))
-      (setenv "PATH" (concat (getenv "PATH") ":" homebrew-bin-dir)))))
-
 (provide 'init-macos)
