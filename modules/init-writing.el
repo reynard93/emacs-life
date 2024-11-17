0b46b626a7c1c4f5a5b3734773044219ec3b8a48
Yejun Su <goofan.su@gmail.com>
2024-11-17 17:43:28 +0800

    merge init-{citar,denote,org}.el into init-writing.el

--- modules/init-writing.el
+++ modules/init-writing.el
@@ -1,4 +1,152 @@
-(require 'init-denote)
-(require 'init-citar)
+(use-package org
+  :ensure nil
+  :bind
+  (("C-c c" . org-capture)
+   ("C-c l" . org-store-link)
+   :map org-mode-map
+   ([remap mark-defun] . org-babel-mark-block)
+   ("M-g o" . consult-org-heading))
+
+  :custom
+  (org-ellipsis "…")
+  (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
+
+  ;; Capture
+  (org-capture-templates
+   '(("c" "New note (with Org)" item (file "notes.org") "- %?")
+     ("d" "New note (with Denote)" plain
+      (file denote-last-path)
+      #'denote-org-capture
+      :no-save t
+      :immediate-finish nil
+      :kill-buffer t
+      :jump-to-captured t)))
+
+  ;; Code block
+  (org-edit-src-content-indentation 0)
+  (org-edit-src-persistent-message nil)
+  (org-src-preserve-indentation t)
+  (org-src-window-setup 'current-window)
+
+  ;; Tag
+  (org-auto-align-tags nil)
+  (org-tags-column 0)
+
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (racket . t)
+     (shell . t)
+     (ruby . t))))
+
+(use-package ox-hugo
+  :pin melpa
+  :after org)
+
+(use-package ox-gfm
+  :pin melpa
+  :after org)
+
+(use-package citar
+  :pin melpa
+  :init
+  (setq org-cite-global-bibliography '("~/Zotero/references.bib"))
+  (setq org-cite-insert-processor 'citar)
+  (setq org-cite-follow-processor 'citar)
+  (setq org-cite-activate-processor 'citar)
+  :bind
+  (("C-c w b o" . citar-open)
+   ("C-c w b c" . citar-create-note))
+  :custom
+  (citar-bibliography org-cite-global-bibliography)
+  (citar-at-point-function #'embark-act))
+
+(use-package citar-embark
+  :pin melpa
+  :after (citar embark)
+  :config
+  (citar-embark-mode 1))
+
+(use-package denote
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (("C-c n n" . denote)
+   ("C-c n N" . denote-type)
+   ("C-c n o" . denote-sort-dired)
+   :map text-mode-map
+   ("C-c n i" . denote-link-or-create)
+   ("C-c n I" . denote-add-links)
+   ("C-c n b" . denote-backlinks)
+   ("C-c n r" . denote-rename-file)
+   ("C-c n R" . denote-rename-file-using-front-matter)
+   ("C-c n f f" . denote-find-link)
+   ("C-c n f b" . denote-find-backlink)
+   :map org-mode-map
+   ("C-c n d l" . denote-org-extras-dblock-insert-links)
+   ("C-c n d b" . denote-org-extras-dblock-insert-backlinks)
+   :map dired-mode-map
+   ("C-c C-d C-i" . denote-link-dired-marked-notes)
+   ("C-c C-d C-r" . denote-dired-rename-marked-files)
+   ("C-c C-d C-k" . denote-dired-rename-marked-files-with-keywords)
+   ("C-c C-d C-f" . denote-dired-rename-marked-files-using-front-matter)
+   :map search-map
+   ("n" . denote-open-or-create)
+   ("j" . denote-journal-extras-new-or-existing-entry))
+  :custom
+  (denote-directory (expand-file-name "notes/" my-src-directory))
+  (denote-journal-extras-title-format 'day-date-month-year)
+  (denote-known-keywords nil)
+  :config
+  (require 'denote-journal-extras)
+  (require 'denote-org-extras)
+  (denote-rename-buffer-mode 1))
+
+(use-package consult-denote
+  :init
+  (with-eval-after-load 'denote
+    (consult-denote-mode 1))
+  :bind ("C-c n g" . consult-denote-grep)
+  :custom
+  (consult-denote-grep-command #'consult-ripgrep))
+
+(use-package citar-denote
+  :pin melpa
+  :init
+  (with-eval-after-load 'denote
+    (citar-denote-mode 1))
+  :bind
+  (("C-c w b n" . citar-denote-open-note)
+   :map org-mode-map
+   ("C-c w b d" . citar-denote-dwim)
+   ("C-c w b e" . citar-denote-open-reference-entry)
+   ("C-c w b k" . citar-denote-add-citekey)
+   ("C-c w b K" . citar-denote-remove-citekey))
+  :custom
+  (citar-denote-subdir "reference"))
+
+(use-package denote-explore
+  :pin melpa
+  :bind
+  (;; Statistics
+   ("C-c w x c" . denote-explore-count-notes)
+   ("C-c w x C" . denote-explore-count-keywords)
+   ("C-c w x b" . denote-explore-barchart-keywords)
+   ("C-c w x e" . denote-explore-barchart-filetypes)
+   ;; Random walks
+   ("C-c w x r" . denote-explore-random-note)
+   ("C-c w x l" . denote-explore-random-link)
+   ("C-c w x k" . denote-explore-random-keyword)
+   ("C-c w x x" . denote-explore-random-regex)
+   ;; Denote Janitor
+   ("C-c w x d" . denote-explore-identify-duplicate-notes)
+   ("C-c w x z" . denote-explore-zero-keywords)
+   ("C-c w x s" . denote-explore-single-keywords)
+   ("C-c w x o" . denote-explore-sort-keywords)
+   ("C-c w x w" . denote-explore-rename-keyword)
+   ;; Visualise denote
+   ("C-c w x n" . denote-explore-network)
+   ("C-c w x v" . denote-explore-network-regenerate)
+   ("C-c w x D" . denote-explore-degree-barchart)))
 
 (provide 'init-writing)
