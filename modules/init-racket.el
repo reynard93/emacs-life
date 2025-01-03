c1f4524e2a586b91dbcdaa340e483d846fe52e75
Yejun Su <goofan.su@gmail.com>
2025-01-03 23:21:33 +0800

    set default lang to sicp for racket org-babel

--- modules/init-racket.el
+++ modules/init-racket.el
@@ -4,6 +4,12 @@
 
 (use-package ob-racket
   :vc (ob-racket :url "https://github.com/hasu/emacs-ob-racket.git")
+  :after org
+  :custom
+  (ob-racket-default-lang "sicp"))
+
+(use-package sicp
+  :pin melpa
   :defer t)
 
 (provide 'init-racket)
