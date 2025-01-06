5e9fcf0c986b0c412ff2c7e27a0393d1607d5b3f
Yejun Su <goofan.su@gmail.com>
2025-01-06 23:10:38 +0800

    sort treesit-language-source-alist

--- modules/init-tree-sitter.el
+++ modules/init-tree-sitter.el
@@ -2,16 +2,16 @@
   :ensure nil
   :init
   (setq treesit-language-source-alist
-        '((ruby "https://github.com/tree-sitter/tree-sitter-ruby")
+        '((c "https://github.com/tree-sitter/tree-sitter-c")
+          (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
           (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
           (heex "https://github.com/phoenixframework/tree-sitter-heex")
-          (css "https://github.com/tree-sitter/tree-sitter-css")
           (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
           (json "https://github.com/tree-sitter/tree-sitter-json")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
           (toml "https://github.com/tree-sitter/tree-sitter-toml")
-          (c "https://github.com/tree-sitter/tree-sitter-c")
-          (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-          (python "https://github.com/tree-sitter/tree-sitter-python")))
+          (ruby "https://github.com/tree-sitter/tree-sitter-ruby")))
 
   :custom
   (major-mode-remap-alist
