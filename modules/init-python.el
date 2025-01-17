f29855a029c3af11d777b760a85aca546d9d456d
Yejun Su <goofan.su@gmail.com>
2025-01-17 15:11:50 +0800

    use ruff-format

--- modules/init-python.el
+++ modules/init-python.el
@@ -1,9 +1,5 @@
-(use-package python
-  :ensure nil
-  :hook ((python-mode python-ts-mode) . python-format-before-save)
-  :config
-  (defun python-format-before-save ()
-    (when (derived-mode-p 'python-base-mode)
-      (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
+(use-package ruff-format
+  :pin melpa
+  :hook ((python-mode python-ts-mode) . ruff-format-on-save-mode))
 
 (provide 'init-python)
