4e476884fbafef8969ba89ace7dfd5c82efb5082
Yejun Su <goofan.su@gmail.com>
2025-01-24 17:34:54 +0800

    add my/homebrew-casks-insert

--- modules/init-nix.el
+++ modules/init-nix.el
@@ -1,6 +1,7 @@
 (use-package nix-mode
   :pin nongnu
   :hook (before-save . nix-format-before-save)
+  :bind (:map my-insert-map ("n n" . my/homebrew-casks-insert))
   :config
   (defun org-babel-execute:nix (body params)
     (setq strict-option (if (assoc :strict params) "--strict" ""))
@@ -11,7 +12,14 @@
        (concat "nix-instantiate --eval " strict-option " - <<EOF\n$(cat)\nEOF")
        (current-buffer)
        t)
-      (buffer-string))))
+      (buffer-string)))
+
+  (defun my/homebrew-casks-insert ()
+    "Insert currently installed Homebrew Casks at position."
+    (interactive)
+    (let* ((casks (shell-command-to-string "brew list --cask | awk '{print \"\\\"\" $0 \"\\\"\"}'"))
+           (result (concat "[" casks "]")))
+      (insert result))))
 
 (use-package org-nix-shell
   :pin melpa
