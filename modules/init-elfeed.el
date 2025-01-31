c0e6ab32f0a1707c07be17e1e3db0c30620f4229
Yejun Su <goofan.su@gmail.com>
2025-01-31 18:14:03 +0800

    update elfeed-feeds

--- modules/init-elfeed.el
+++ modules/init-elfeed.el
@@ -3,6 +3,7 @@
   :bind ("C-c e" . elfeed)
   :custom
   (elfeed-feeds
-   '("https://sachachua.com/blog/category/emacs-news/feed/")))
+   '("https://sachachua.com/blog/category/emacs-news/feed/"
+     "https://world.hey.com/this.week.in.rails/feed.atom")))
 
 (provide 'init-elfeed)
