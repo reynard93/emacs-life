b71bb20658a3a7cde19dd2bf209902ac29491287
Yejun Su <goofan.su@gmail.com>
2025-02-07 00:39:38 +0800

    [gptel] add openrouter models

--- modules/init-gpt.el
+++ modules/init-gpt.el
@@ -16,7 +16,10 @@
       :stream t
       :key (lambda () (auth-source-pass-get 'secret "api-key/openrouter"))
       :models '(anthropic/claude-3.5-sonnet
-                openai/gpt-4o-mini)))
+                anthropic/claude-3.5-haiku
+                openai/gpt-4o
+                openai/gpt-4o-mini
+                openai/o1-mini)))
 
   (defvar gptel--deepseek
     (gptel-make-openai "DeepSeek"
