7e603b57616df663f114f84c3e27869bdf31993f
Yejun Su <goofan.su@gmail.com>
2025-02-27 21:52:16 +0800

    claude-3.5-sonnet -> claude-3.7-sonnet

--- modules/init-gpt.el
+++ modules/init-gpt.el
@@ -15,9 +15,8 @@
       :endpoint "/api/v1/chat/completions"
       :stream t
       :key (lambda () (auth-source-pass-get 'secret "api-key/openrouter"))
-      :models '(anthropic/claude-3.5-sonnet
-                openai/gpt-4o-mini
-                openai/o3-mini-high)))
+      :models '(anthropic/claude-3.7-sonnet
+                openai/gpt-4o-mini)))
 
   :bind
   (("C-c <return>" . gptel-send)
