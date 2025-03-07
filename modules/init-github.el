896882e339f59a74c6bc577db224bfca5e0ba5a9
reynard93 <reynard.lee@thoughtworks.com>
2025-03-07 23:35:32 +0800

    add consult-h Note to self: just try denote

--- modules/init-github.el
+++ modules/init-github.el
@@ -7,4 +7,24 @@
    :map embark-region-map
    ("G" . gh-gist-create)))
 
+(use-package consult-gh
+  :after consult
+  :custom
+  (consult-gh-default-clone-directory "~/src/")
+  (consult-gh-repo-action #'consult-gh--repo-browse-files-action)
+  (consult-gh-issue-action #'consult-gh--issue-view-action)
+  (consult-gh-pr-action #'consult-gh--pr-view-action)
+  (consult-gh-code-action #'consult-gh--code-view-action)
+  (consult-gh-file-action #'consult-gh--files-view-action)
+  (consult-gh-large-file-warning-threshold 2500000)
+  :config
+  (add-to-list 'savehist-additional-variables 'consult-gh--known-orgs-list)
+  (add-to-list 'savehist-additional-variables 'consult-gh--known-repos-list)
+  (consult-gh-enable-default-keybindings))
+
+(use-package consult-gh-embark
+  :after consult-gh
+  :config
+  (consult-gh-embark-mode 1))
+
 (provide 'init-github)
