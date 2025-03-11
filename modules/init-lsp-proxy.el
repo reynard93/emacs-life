1d9623ef674c07307a49848af709e96912c74728
reynard93 <reynard.lee@thoughtworks.com>
2025-03-11 15:29:18 +0800

    Add ruby-lsp server for lsp-proxy, configure the languages.toml file

--- modules/init-lsp-proxy.el
+++ modules/init-lsp-proxy.el
@@ -6,10 +6,12 @@
                  :files ("lsp-proxy.el" "lsp-proxy")
                  )
   :config
+  (setq lsp-proxy-log-level 3)
   (add-hook 'tsx-ts-mode-hook #'lsp-proxy-mode)
   (add-hook 'js-ts-mode-hook #'lsp-proxy-mode)
   (add-hook 'typescript-mode-hook #'lsp-proxy-mode)
   (add-hook 'typescript-ts-mode-hook #'lsp-proxy-mode)
+  (add-hook 'ruby-ts-mode-hook #'lsp-proxy-mode)
   )
 
 (provide 'init-lsp-proxy)
