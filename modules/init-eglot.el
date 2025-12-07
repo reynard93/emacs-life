26a82f292a9e8fc0d4355ae809c099219f09bbaa
Reynard Lee <yylee.rey@gmail.com>
2025-12-07 11:49:33 +0800

    very very simple eglot

--- modules/init-eglot.el
+++ modules/init-eglot.el
@@ -14,14 +14,7 @@
   (eglot-events-buffer-size 0) ; Disable events buffer for performance
   (eglot-use-plists t) ; Use plists for better performance
   :config
-  ;; Helper function to find ruby-lsp or the bundle version
-  (defun my-eglot-find-ruby-lsp ()
-    "Find ruby-lsp executable, preferring project version then our wrapper."
-    (or (executable-find "bundle")
-        (expand-file-name "bin/ruby-lsp user-emacs-directory")
-        "/opt/homebrew/share/gems/bin/ruby-lsp"))
-
-  ;; Configure servers for languages that are available
+  ;; Helper function to find ruby-lsp executable
   (dolist (mode '(((c-ts-mode c++-ts-mode) . ("clangd"
                                               "--background-index"
                                               "--clang-tidy"
@@ -30,12 +23,11 @@
                                               "--pch-storage=memory"
                                               "--log=error"))
                   ((rust-ts-mode) . ("rust-analyzer"))
-                  ((ruby-mode ruby-ts-mode) . (lambda () ; Use lambda for dynamic server
-                                               (list (my-eglot-find-ruby-lsp)))))
-    (add-to-list 'eglot-server-programs mode))
+                  ; ((ruby-mode ruby-ts-mode) . (my-eglot-ruby-lsp-command))))
+    (add-to-list 'eglot-server-programs mode))))
 
   ;; Fix completion ordering and behavior
-  (setq completion-category-overrides '((eglot (styles orderless flex))))))
+  (setq completion-category-overrides '((eglot (styles orderless flex)))))
 
 ;; https://github.com/jdtsmith/eglot-booster (i installed with M-x package-vc-install
 ;; both lsp-mode and eglot uses this -> https://github.com/blahgeek/emacs-lsp-booster
@@ -49,17 +41,8 @@
   :ensure t
   :after (eglot consult)
   :bind (:map eglot-mode-map
-            ("C-M-." . consult-eglot-symbols)
+            ("C-M-." . consult-eglot-symbol
             ("C-c l r" . consult-eglot-references)
             ("C-c l a" . consult-eglot-code-actions)))
-
-;; Configure eglot keybindings similar to lsp-mode
-(defun my-eglot-setup-keys ()
-  "Set up keybindings for eglot mode."
-  (local-set-key (kbd "C-c C-d") 'eglot-doc-at-point)
-  (local-set-key (kbd "M-g .") 'xref-find-definitions)
-  (local-set-key (kbd "M-g ,") 'xref-go-back))
-
-(add-hook 'eglot-managed-mode-hook #'my-eglot-setup-keys)
 
 (provide 'init-eglot)
