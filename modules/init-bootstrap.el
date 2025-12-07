530a9cddd7ff993f4fafac8420c80ae10ff46c40
Reynard Lee <yylee.rey@gmail.com>
2025-12-07 13:49:36 +0800

    add mise pacakge

--- modules/init-bootstrap.el
+++ modules/init-bootstrap.el
@@ -5,10 +5,20 @@
 
 (require 'init-elpaca)
 
-;; Set $PATH correctly
-;; other ways: https://www.reddit.com/r/emacs/comments/1fxha3a/homebrew_emacs_plus_path_issue_and_solutions_in/
-;; Enabling `envrc-global-mode'
-(use-package envrc
-  :hook (after-init . envrc-global-mode))
+;; Set $PATH correctly using mise.el for version manager integration
+;; mise.el sets environment variables (PATH, GEM_HOME, etc.) per-buffer
+;; based on mise configuration files (.mise.toml, .tool-versions)
+;; https://github.com/eki3z/mise.el
+(use-package mise
+  :ensure (:host github :repo "liuyinz/mise.el")
+  :demand t  ; Load immediately, not lazily
+  :config
+  ;; Ensure mise can find the executable
+  (setq mise-executable (or (executable-find "mise")
+                            "/opt/homebrew/bin/mise"))
+  ;; Enable debug mode temporarily to diagnose issues
+  ;; (setq mise-debug t)
+  ;; Enable global mode immediately after config
+  (global-mise-mode 1))
 
 (provide 'init-bootstrap)
