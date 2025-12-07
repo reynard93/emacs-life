260355e04bd7b63e2fe5f9b4efd3ffcd15fdaed3
Reynard Lee <yylee.rey@gmail.com>
2025-12-07 11:04:23 +0800

    change from lsp mode to eglot

--- modules/init-programming.el
+++ modules/init-programming.el
@@ -19,8 +19,8 @@
   :diminish
   :hook (after-init . editorconfig-mode))
 
-;; (require 'init-eglot)
-(require 'init-lsp)
+(require 'init-eglot)
+;; (require 'init-lsp)
 (require 'init-data)
 (require 'init-docker)
 ;; (require 'init-c)
