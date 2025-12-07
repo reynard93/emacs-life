762090b83589d64a5e3d48a35ab3c8af745cbf30
Reynard Lee <yylee.rey@gmail.com>
2025-12-07 20:17:01 +0800

    fix: add rg i.e. allow homebrew bins to be discovered

--- early-init.el
+++ early-init.el
@@ -5,7 +5,8 @@
 (tool-bar-mode -1)
 
 ;; Fix for native compilation on macOS with MacPorts libgccjit
-(setenv "PATH" (concat "/opt/local/bin:" (getenv "PATH")))
+(setenv "PATH" (concat "/opt/homebrew/bin:/opt/local/bin:" (getenv "PATH")))
+(add-to-list 'exec-path "/opt/homebrew/bin")
 (setenv "SDKROOT" (shell-command-to-string "xcrun --show-sdk-path | tr -d '\n'"))
 (setq native-comp-driver-options '("-B/opt/local/bin" "-I/opt/local/include/gcc14" "-L/opt/local/lib/gcc14"))
 
