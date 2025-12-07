2584ac4829ada13a029db8d5215fabed9d51cb3e
Reynard Lee <yylee.rey@gmail.com>
2025-12-07 17:16:09 +0800

    goggles + pulse functionality already in pulsar
    .

--- modules/init-programming.el
+++ modules/init-programming.el
@@ -20,11 +20,9 @@
   :hook (after-init . editorconfig-mode))
 
 (require 'init-eglot)
-;; (require 'init-lsp)
 (require 'init-data)
 (require 'init-docker)
 (require 'init-markdown)
-(require 'init-python)
 (require 'init-ruby)
 (require 'init-web)
 (require 'init-yaml)
